9d298b9c46367c116239a22a971dc9f69af2641a
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2015-10-15 19:33:04 +0200

    Voting view updated. Iñigo Molinuevo

--- src/main/java/apaw/ecp2/presentation/views/VotingView.java
+++ src/main/java/apaw/ecp2/presentation/views/VotingView.java
@@ -1,12 +1,21 @@
 package apaw.ecp2.presentation.views;
 
+import java.util.Map.Entry;
+
+import apaw.ecp2.business.views.VoteTransferObject;
 import apaw.ecp2.presentation.models.Model;
 
-public class VotingView implements View{
+public class VotingView implements View {
 
 	@Override
 	public void show(Model model) {
 		System.out.println("Voting Page");
+		System.out.print("   Temas: [");
+		for (Entry<String, Object> e : model.getMap().entrySet()) {
+			System.out.print("["
+					+ ((VoteTransferObject) e.getValue()).getThemeName() + ":"
+					+ ((VoteTransferObject) e.getValue()).getValue() + "]");
+		}
+		System.out.println("]");
 	}
-
 }
